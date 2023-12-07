fa0d8fc2f98563964bd78b4f2c75cabe542a85b8
alexis piekarz <alexis.piekarz@etudiant.univ-lr.fr>
2023-12-07 22:52:23 +0100

    rien marche GL

--- backend/index.js
+++ backend/index.js
@@ -10,15 +10,35 @@ const io = require("socket.io")(server, {
   }
 });
 
-
-app.get('/', (req, res) => {
-  res.send('<h1>Backend</h1><h2>T\'es pas cense etre la mon reuf</h2>');
-});
-
 io.on('connection', (socket) => {
-  console.log(`A userconnected: ${socket.id}`);
+  console.log('A user connected:', socket.id);
+
+  socket.on('createRoom', () => {
+    const newRoom = generateRandomRoomName();
+    socket.join(newRoom);
+    rooms.set(socket.id, { room: newRoom });
+    console.log(`User ${socket.id} created and joined room: ${newRoom}`);
+    io.to(socket.id).emit('roomCreated', newRoom);
+  });
+
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    rooms.set(socket.id, { room });
+    console.log(`User ${socket.id} joined room: ${room}`);
+    io.to(room).emit('roomChanged', room);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    rooms.delete(socket.id);
+  });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+function generateRandomRoomName() {
+  return Math.random().toString(36).substring(2, 7);
+}
--- frontend/n2i-frontend/src/routes/+page.svelte
+++ frontend/n2i-frontend/src/routes/+page.svelte
@@ -1,25 +1,58 @@
-<svelte:head>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet"> 
-</svelte:head>
-
-<Card isFlipped="true"/>
-
 
 <script>
-
-import ioClient from 'socket.io-client';
-const ENDPOINT = 'http://localhost:3000';
-
-const socket = ioClient(ENDPOINT);
-export const io = socket;
-
-import Card from '../components/card/card.svelte';
-</script>
-
-
-<style>
-    :global(body) {
-        font-family: 'Nunito';
-    }
-</style>
-
+    import { onMount, onDestroy, beforeUpdate } from 'svelte';
+    import ioClient from 'socket.io-client';
+    import { writable } from 'svelte/store';
+  
+    const ENDPOINT = 'http://localhost:3000';
+    const roomInfo = writable({ createdRoom: '', canJoinRoom: false });
+  
+    let socket;
+  
+    onMount(() => {
+      socket = ioClient(ENDPOINT);
+  
+      socket.on('connect', () => {
+        console.log('Connecté au serveur :', socket.id);
+      });
+  
+      socket.on('disconnect', () => {
+        console.log('Déconnecté du serveur');
+      });
+  
+      socket.on('roomCreated', (room) => {
+        console.log('Salle créée :', room);
+        roomInfo.update(info => ({ ...info, createdRoom: room, canJoinRoom: true }));
+      });
+    });
+  
+    beforeUpdate(() => {
+      if (socket) {
+        socket.disconnect();
+      }
+    });
+  
+    const createRoom = () => {
+      socket.emit('createRoom');
+    };
+  
+    const joinRoom = () => {
+      const { createdRoom, canJoinRoom } = $roomInfo;
+      if (canJoinRoom) {
+        socket.emit('joinRoom', createdRoom);
+      }
+    };
+  </script>
+  
+  <h1>Bienvenue sur SvelteKit</h1>
+  <p>Visitez <a href="https://kit.svelte.dev">kit.svelte.dev</a> pour lire la documentation</p>
+  
+  <div>
+    <button on:click={createRoom}>Créer une salle</button>
+  
+    {#if $roomInfo.createdRoom}
+      <p>Salle créée : {$roomInfo.createdRoom}</p>
+      <button on:click={joinRoom} disabled={!$roomInfo.canJoinRoom}>Rejoindre la salle</button>
+    {/if}
+  </div>
+  
