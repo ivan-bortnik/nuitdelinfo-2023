30590a3a79e9fa6731ae889a90d7f5128f15e94c
Ivan Bortnik <ivan.bortnik@etudiant.univ-lr.fr>
2023-12-07 19:11:26 +0100

    hello World (fontend)

---
+++ frontend/n2i-frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<script>
+
+import ioClient from 'socket.io-client';
+const ENDPOINT = 'http://localhost:3000';
+
+const socket = ioClient(ENDPOINT);
+
+export const io = socket;
+
+</script>
